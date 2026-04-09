b5cf90a55642ee275e7c55eae8d6cb872a5ac6a7
Sebastien <sebastiendumont1986@gmail.com>
2026-04-09 16:38:07 +0200

    Removed unused _auth field and AuthManager import

--- lib/src/resources/sessions_resource.dart
+++ lib/src/resources/sessions_resource.dart
@@ -1,4 +1,3 @@
-import '../auth/auth_manager.dart';
 import '../http/http_client.dart';
 import '../http/response.dart';
 
@@ -7,9 +6,8 @@ import '../http/response.dart';
 /// Requires admin/consumer key authentication.
 class SessionsResource {
   final CoCartHttpClient _http;
-  final AuthManager _auth;
 
-  SessionsResource(this._http, this._auth);
+  SessionsResource(this._http);
 
   Future<CoCartResponse> all() => _http.get('sessions');
 }
